3c3b5a683f949a31a612c9fd2bc2c94cbaa1816b
Rafael Mori <faelmori@gmail.com>
2025-02-06 22:48:44 +0300

    Fix some things.
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- service.go
+++ service.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a zlib-style
 // license that can be found in the LICENSE file.
 
-// Package service provides a simple way to create a system service.
+// Package keepgo provides a simple way to create a system service.
 // Currently supports Windows, Linux/(systemd | Upstart | SysV | OpenRC), and OSX/Launchd.
 //
 // Windows controls services by setting up callbacks that is non-trivial. This
